a6a265c0c3c4aca781efbe4919582af735f936c8
rchellakumar <ckchellakumar2003@gmail.com>
2025-06-21 17:18:40 +0530

    local proxy server was initialized

--- server.js
+++ server.js
@@ -1,21 +1,31 @@
-// server.js
 const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
+const https = require("https");
 require("dotenv").config();
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 const API_KEY = process.env.TMDB_API_KEY;
 
 app.get("/api/movies", async (req, res) => {
+  const query = req.query.query;
+
+  const tmdbUrl = query
+    ? `https://api.themoviedb.org/3/search/movie?query=${encodeURIComponent(
+        query
+      )}`
+    : `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc`;
   try {
-    const response = await axios.get("https://api.themoviedb.org/3/movie/popular", {
-      params: {
-        api_key: API_KEY,
-        language: "en-US",
-        page: req.query.page || 1,
+    const response = await axios.get(tmdbUrl, {
+      headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${API_KEY}`,
       },
     });
     res.json(response.data);
@@ -24,6 +34,26 @@ app.get("/api/movies", async (req, res) => {
     res.status(500).json({ error: "TMDb fetch failed" });
   }
 });
+
+
+app.get(/^\/api\/image\/(.+)$/, (req, res) => {
+  const posterpath = req.params[0]; // the captured filename
+
+  if (!posterpath) {
+    return res.status(400).send("Missing image poster path");
+  }
+
+  const imageUrl = `https://image.tmdb.org/t/p/w500/${posterpath}`;
+  https.get(imageUrl, (imageRes) => {
+    res.setHeader("Content-Type", imageRes.headers["content-type"]);
+    imageRes.pipe(res);
+  }).on("error", (err) => {
+    console.error("Image fetch failed:", err.message);
+    res.status(500).send("Image fetch failed");
+  });
+});
+
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
